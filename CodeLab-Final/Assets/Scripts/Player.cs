0503fdd5a9578829fbd4b2e7ba1d47aebcd3e25a
SamuelSords <54908881+SamuelSords@users.noreply.github.com>
2020-05-13 19:08:44 -0400

    shooting stars, player control, stage set

---
+++ CodeLab-Final/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move character
+        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+        transform.Translate(0f, Input.GetAxis("Vertical") * 15f * Time.deltaTime, 0f);
+
+        // flip character
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -5;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 5;
+        }
+        transform.localScale = characterScale;
+    }
+}
+
---
+++ CodeLab-Final/Assets/Scripts/StarScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarScript : MonoBehaviour
+{
+    public float yRange; //random range for star pos on the x axis
+
+    const float MIN_SPEED = 0.05f; //min speed
+    const float MAX_SPEED = 0.3f; //max speed
+    const float MAX_X = 12; //where to start star
+    const float MIN_X = -10; //wher to recycle star
+
+    float speed;
+    private Vector3 scaleChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset(); //setup star
+    }
+
+    public void Reset()
+    {
+        speed = -Random.Range(MIN_SPEED, MAX_SPEED); //give it a random speed in range
+
+        scaleChange = new Vector3(.003f, .003f, .003f);
+
+        transform.localScale = new Vector3(.25f, .25f, 1f);
+
+        //set random star pos
+        transform.position = new Vector2(
+            MAX_X, Random.Range(2, yRange));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move star by speed
+        transform.position = new Vector3(
+            transform.position.x + speed,
+            transform.position.y);
+
+        transform.localScale -= scaleChange;
+        //transform.localScale = new Vector3(transform.localScale.x - scaleChange.x, transform.localScale.y - scaleChange.y);
+
+        //if the star has gone to far
+        if (transform.position.x < MIN_X)
+        {
+            StarPool.instance.Push(gameObject); //recycle it into the pool
+        }
+
+        if (transform.localScale.x < 0f)
+        {
+            StarPool.instance.Push(gameObject);
+        }
+    }
+}
